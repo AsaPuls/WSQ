7b1e86997cf930c0267e6cbabb215ee7891a0c89
AsaPuls <asa.puls@gmail.com>
2015-02-27 14:54:51 -0600

    Update WSQ09.cpp

--- WSQ09.cpp
+++ WSQ09.cpp
@@ -10,11 +10,11 @@ int Factorial(int number){    // This is a seperate function which
 int main (){
   char response;
   cout << "Would you like to calculate the factorial of a number?" << endl << "Y or N" << endl;
-  cin >> response;
-  while (response == 'Y'){
+  cin >> response;            // Inputs response; Either starts loop or end the program
+  while (response == 'Y'){    // If response is yes: Starts a loop
     int number;
     cout << "Give me a number to calculate the factorial of:" << endl << "The number must be an integer between 1 and 12: ";
-    cin >> number;
+    cin >> number;             // Input the number you want to calculate the factorial of
     if (number>0 && number<13)
       cout << "The factorial of the number " << number << " is: " << Factorial(number) << endl;
     if (number<=0)            // You can not have a factorial of a negative number
@@ -23,8 +23,8 @@ int main (){
       cout << "The number must be less than 13 or else the result will not be accurate" << endl;
     cout << "Would you like to calculate the factorial of another number?" << endl << "Y or N" << endl;
     cin >> response;          // Changes the response, creates a place to exit the loop
-  }
-  if (response == 'N')
+  }                           // If there was no place to change the response the loop will never end
+  if (response == 'N')        // If response is no: Prints text and ends program
     cout << "Then why did you open this program!" << endl << "You fucking idiot!" << endl;
   return 0;                   // The only place which exits the main program is here
 }

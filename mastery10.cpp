2f5b74e980b36ebcd95d83753e7746ae14aec7cd
AsaPuls <asa.puls@gmail.com>
2015-02-27 10:11:28 -0600

    Create mastery10.cpp

---
+++ mastery10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+  double num;
+  cout << "Printing random words to complete mastery 10" << endl;
+  cout << "Give me a number so you can get credit for mastery 10" << endl;
+  cin >> num;
+  cout << "The number you inputted was: " << num << endl;
+  return 0;
+}

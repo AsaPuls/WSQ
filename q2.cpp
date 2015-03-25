9dafbd1ae2cef1054e35fb97b8b7b28dd84840ab
AsaPuls <asa.puls@gmail.com>
2015-03-24 23:41:07 -0600

    Create q2.cpp

---
+++ q2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void tri(int size){
+  string letters;
+  for (int i=0; i<size; i++){
+    letters = letters + "T";
+    cout << letters << endl;
+  }
+  for(int i=(size-1); i>0; i--){
+    for(int j=1; j<=i; j++){
+      cout << "T" ;
+    }
+    cout << endl;
+  }
+}
+int main(){
+  int size;
+  cout << "Give me the size of the triangle: ";
+  cin >> size;
+  tri(size);
+  return 0;
+}
